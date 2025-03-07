99c745e79db21f485a9351a5f943815a2c1fa826
Mike <michael@obsessvr.com>
2025-03-06 23:07:19 -0500

    adding cloudflare worker.js file and deploying a route https://internet-speedtest-worker.miketu926.workers.dev/

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 
+// Project Structure
+// Let's start by setting up a Vite React project and planning the components needed:
+
+// Create a new Vite+React project
+// Set up Cloudflare Workers for handling the speed test
+// Build a simple UI with a "Start Test" button and results display
+
+// How Speed Tests Work
+// Speed tests typically work by:
+
+// Downloading files of known sizes
+// Measuring how long it takes to download them
+// Calculating the speed (bytes/second)
+
 function App() {
   return (
     <div>
